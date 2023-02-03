e71e9697e239ffc2fc74bd3562b56857af9e2760
rahul <shar789@usc.edu>
2023-02-02 22:30:09 -0800

    face detector

---
+++ src/ASD/ASD_framework.py
@@ -0,0 +1,8 @@
+
+class ASD():
+    def __init__(self, speechFaceTracks, faceTracks, faceFeatures, speechFeatures):
+        self.speechFaceTracks = speechFaceTracks
+        self.faceTracks = faceTracks
+        self.faceFeatures = faceFeatures
+        self.speechFeatures = speechFeatures
+        
