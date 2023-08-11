8c1adfa382a9c852e234d8a88a09a169a358be5f
Rahul Sharma <shar789@usc.edu>
2023-08-11 07:35:12 -0700

    Update setup.sh
    adding the local bin path for scenedetect in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 # download the Keras_vgg_face_2 model
 gdown 11JQyEfCdo7cqA8IDbUa8JadVK65YG_BA -O Keras_VGGFace2_ResNet50/weights/
 gdown 11C_g1bwOWCU13T6Gf2FDVt8AhLKTN64N -O Pytorch_VGGFace2_ResNet50/weights/
+export PATH=~/.local/bin:$PATH
